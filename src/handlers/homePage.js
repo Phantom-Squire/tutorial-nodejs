8610088e7bb4ccb2bcf17371e1b67a221ef2ca9b
Brian Leyva <55462869+Phantom-Squire@users.noreply.github.com>
2020-03-11 21:52:57 -0700

    Update homePage.js

--- src/handlers/homePage.js
+++ src/handlers/homePage.js
@@ -1,10 +1,28 @@
-const getTodosByFilter = require('../services/getTodosByFilter');
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Exploring Trophic Cascades</title>
+  <base href="./">
 
-const homePage = async (req, res) => {
-  const data = await getTodosByFilter(req.params.shown);
-  res.render('index', data);
-};
-
-module.exports = {
-  homePage,
-};
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link rel="icon" type="image/x-icon" href="https://www.hhmi.org/sites/all/themes/bio_theme/favicon.ico"> -->
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-WRC3MJ');</script>
+  <!-- End Google Tag Manager -->
+<link rel="stylesheet" href="styles.ffcb0716198f9d8fdc29.css"></head>
+<body>
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WRC3MJ"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <app-root></app-root>
+  <div role="region" lang="en" class="scene-summary">
+    <p aria-live="polite" role="status" id="interactiveText"></p>
+  </div>
+<script type="text/javascript" src="runtime.26209474bfa8dc87a77c.js"></script><script type="text/javascript" src="polyfills.bebee6a5ef0ece001bc6.js"></script><script type="text/javascript" src="scripts.49dd82cc156cfb5dfa85.js"></script><script type="text/javascript" src="main.fbdddd6df5bb4593c7c3.js"></script></body>
+</html>
